1c9bfa81cee6bbe39b7a8ed713d297797b74d859
tigeryu8900 <tigeryu8900@gmail.com>
2023-06-14 20:41:33 -0700

    support fromQuery for anime-planet

--- ap.js
+++ ap.js
@@ -3,34 +3,162 @@ const jquery = require("jquery");
 
 const wdl = require("./wdl");
 
-async function getLink(query, category) {
-  try {
-    let $ = jquery(
-        (await JSDOM.fromURL(`https://www.anime-planet.com/${category}/all?` +
-            new URLSearchParams({name: query}))).window);
-    // [...$(`ul[data-type="${category}"] > li > a`)]
-    // .map(e => [$(e).find('.cardName').text(), e.href])
-    // .reduce((acc, x) => ({...acc, [x[0]]: x[1]}), {})
-    let entry = [...$(`ul[data-type="${category}"] > li > a`)].reduce(
+function getEntry(entries, query) {
+  let entry = [...entries].reduce(
+      (acc, a) => {
+        let frag = JSDOM.fragment(a.title.trim());
+        let english = frag.querySelector('h5').textContent.trim();
+        let romaji = frag.querySelector('h6').textContent.slice(11).trim();
+        let distance = Math.min(wdl(query, english), wdl(query, romaji));
+        return (distance < acc.distance) ? {
+          link: a.href,
+          title: romaji,
+          frag,
+          distance
+        } : acc;
+      }, {distance: Infinity}
+  );
+  return entry.distance / query.length < 0.5 ? entry : null;
+}
+
+async function fromQuery(query, category) {
+  let win = (await JSDOM.fromURL(`https://www.anime-planet.com/${category}/all?` +
+      new URLSearchParams({name: query}))).window;
+  let $ = jquery(win);
+  if (win.location.pathname === "/anime/all") {
+    let entry = [...$(`li[data-type="${category}"] > a`)].reduce(
         (acc, a) => {
-          let distance = wdl(query, $(a).find('.cardName').text());
+          let frag = JSDOM.fragment(a.title);
+          let english = frag.querySelector('h5').textContent;
+          let romaji = frag.querySelector('h6').textContent.slice(11);
+          let [, format, episodes] = frag.querySelector('li.type').textContent.match(/^(.*?)\s*\((\d+) \w+\)$/);
+          episodes = parseInt(episodes);
+          let distance = Math.min(wdl(query, english), wdl(query, romaji));
           return (distance < acc.distance) ? {
             link: a.href,
+            title: romaji,
+            frag,
             distance
           } : acc;
         }, {distance: Infinity});
-    if (!entry) {
-      return null;
-    }
-    return entry.distance / query.length < 0.5 ? entry.link : null;
-  } catch (e) {
-    return null;
   }
 }
 
+// async function getLink(query, category) {
+//   try {
+//     let win = (await JSDOM.fromURL(`https://www.anime-planet.com/${category}/all?` +
+//         new URLSearchParams({name: query}))).window;
+//     let $ = jquery(win);
+//     if (win.location.pathname === "/anime/all") {
+//       // [...$(`ul[data-type="${category}"] > li > a`)]
+//       // .map(e => [$(e).find('.cardName').text(), e.href])
+//       // .reduce((acc, x) => ({...acc, [x[0]]: x[1]}), {})
+//       let entry = [...$(`ul[data-type="${category}"] > li > a`)].reduce(
+//           (acc, a) => {
+//             let distance = wdl(query, $(a).find('.cardName').text());
+//             return (distance < acc.distance) ? {
+//               link: a.href,
+//               distance
+//             } : acc;
+//           }, {distance: Infinity});
+//       if (!entry) {
+//         return null;
+//       }
+//       return entry.distance / query.length < 0.5 ? entry.link : null;
+//     } else {}
+//   } catch (e) {
+//     return null;
+//   }
+// }
+
+async function getLink(query, category) {
+  let win = (await JSDOM.fromURL(`https://www.anime-planet.com/${category}/all?` +
+      new URLSearchParams({name: query}))).window;
+  if (win.location.pathname === `/${category}/all`) {
+    return getEntry(jquery(win)(`ul[data-type="${category}"] > li > a`), query).link;
+  } else {
+    return win.location.href;
+  }
+}
+
+const animePattern = /\s*(.*?)\s*\((\d+(\+?)) \w+\)/;
+const mangaPattern = /Vol:\s*(\d+(\+?))\s*;\s*Ch:\s*(\d+(\+?))/;
+
 module.exports = {
+  async animeFromQuery(query) {
+    let win = (await JSDOM.fromURL(`https://www.anime-planet.com/anime/all?` +
+        new URLSearchParams({name: query}))).window;
+    let $ = jquery(win);
+    if (win.location.pathname === "/anime/all") {
+      let entry = getEntry($(`ul[data-type="anime"] > li > a`), query);
+      if (!entry) {
+        return null;
+      }
+      let frag = entry.frag;
+      let [, format, plus, episodes] = frag.querySelector('.type').textContent.match(animePattern);
+      return {
+        ...entry,
+        format,
+        year: frag.querySelector('.iconYear').textContent.trim(),
+        status: plus ? "Releasing" : "Finished",
+        genres: [...frag.querySelectorAll('.tags > ul > li')].map(li => li.textContent.trim()),
+        description: frag.querySelector('p').innerHTML,
+        episodes: parseInt(episodes),
+        distance: entry.distance
+      };
+    } else {
+      let [, format, plus, episodes] = $('.type').text().match(animePattern);
+      let notes = $('.notes p').html();
+      return {
+        link: win.location.href,
+        title: $('h2.aka').text().slice(11).trim(),
+        format,
+        year: $('.iconYear').text().trim(),
+        status: plus ? "Releasing" : "Finished",
+        description: $('.entrySynopsis p').html() + notes ? `\n<br>\n<br>\n<i>${notes}</i>` : "",
+        episodes: episodes
+      };
+    }
+  },
   async getAnimeLink(query) {
     return getLink(query, "anime");
+  },
+  async mangaFromQuery(query) {
+    let win = (await JSDOM.fromURL(`https://www.anime-planet.com/manga/all?` +
+        new URLSearchParams({name: query}))).window;
+    let $ = jquery(win);
+    if (win.location.pathname === "/manga/all") {
+      let entry = getEntry($(`ul[data-type="manga"] > li > a`), query);
+      if (!entry) {
+        return null;
+      }
+      let frag = entry.frag;
+      let [, volumes, vPlus, chapters, cPlus] = frag.querySelector('.type').textContent.match(mangaPattern);
+      return {
+        ...entry,
+        format: "Manga",
+        year: frag.querySelector('.iconYear').textContent.trim(),
+        status: (vPlus || cPlus) ? "Releasing" : "Finished",
+        genres: [...frag.querySelectorAll('.tags > ul > li')].map(li => li.textContent.trim()),
+        description: frag.querySelector('p').innerHTML,
+        volumes,
+        chapters,
+        distance: entry.distance
+      };
+    } else {
+      let [, volumes, vPlus, chapters, cPlus] = frag.querySelector('.type').textContent.match(mangaPattern);
+      let notes = $('.notes p').html();
+      return {
+        link: win.location.href,
+        title: $('h2.aka').text().slice(11).trim(),
+        format: "Manga",
+        year: $('.iconYear').text().trim(),
+        status: (vPlus || cPlus) ? "Releasing" : "Finished",
+        description: $('.entrySynopsis p').html() + notes ? `\n<br>\n<br>\n<i>${notes}</i>` : "",
+        volumes,
+        chapters
+      };
+    }
   },
   async getMangaLink(query) {
     return getLink(query, "manga");
